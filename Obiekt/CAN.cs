922da9ccc4b5786fc68132b41fc46824fede5cf7
bialek1997 <72503103+bialek1997@users.noreply.github.com>
2021-11-29 13:06:54 +0100

    Obiekt Sterowania

---
+++ Obiekt/CAN.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Obiekt
+{
+    public class CAN
+    {
+        public static double DoObiektu()
+        {
+            try
+            {
+                return Double.Parse(File.ReadAllText("C:\\Users\\lidka\\source\\repos\\DoObiektu.txt"));
+            }
+            catch(IOException)
+            {
+                return -1;
+            }
+            
+        }
+
+        public static void DoSterowania(double value)
+        {
+            try
+            {
+                File.WriteAllText("C:\\Users\\lidka\\source\\repos\\DoSterowania.txt", value.ToString());
+            }
+            catch (IOException)
+            {
+                return;
+            }
+
+        }
+    }
+}
